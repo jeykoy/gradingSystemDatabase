0aa0318ab15e10ccd6525852abad941054570ade
jeykoy <jk_avena@umindanao.edu.ph>
2017-10-03 18:40:50 +0800

    edited Add Grade File

--- SQL Files/addGrade_Proc.sql
+++ SQL Files/addGrade_Proc.sql
@@ -3,9 +3,19 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `addGrade`(IN studID INTEGER, IN subjID INTEGER, IN proID INTEGER, IN inGrade double)
 BEGIN
 
+	DECLARE result INTEGER;
+	
+	/*	1,1,1 
+		1,2,1,
+	 XX 1,1,2*/
+		
+	SELECT count(`gradeID`) INTO result FROM gradesheet WHERE studentID = studID AND subjectID = subjID;
+	
+	IF !(result > 0) THEN
+		IF(inGrade >= 50 && inGrade <=100) THEN
+			INSERT INTO gradesheet(`studentID`,`subjectID`,`profID`,`grade`) VALUES (studID,subjID,proID,inGrade);
+		END IF;
+	END IF;
 
-
-IF(inGrade >= 50 && inGrade <=100) THEN
-INSERT INTO gradesheet(`studentID`,`subjectID`,`profID`,`grade`) VALUES (studID,subjID,proID,inGrade);
-END IF;
+	
 END
